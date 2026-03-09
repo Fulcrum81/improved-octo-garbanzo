5140f4da9315003d427967e0f7127aeda7594187
BelhardTraining <an.maevskaya@gmail.com>
2026-03-09 21:29:26 +0300

    Added successful login test

--- ImprovedOctoGarbanzo/src/Main.java
+++ ImprovedOctoGarbanzo/src/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello World!");
+        System.out.println("I am a login autotest");
+        System.out.println("Trying to login");
+        System.out.println("Username: Vadim");
+        System.out.println("Password: Test1234!");
+        System.out.println("Login successful");
     }
 }
